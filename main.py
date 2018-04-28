8b1a7f43117bcc44591accf272013df54b9da919
paubric <paubric@gmail.com>
2018-04-28 18:33:16 +0300

    range, iqr, var, stddev, r, bugfixes

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--file')
 results = parser.parse_args()
 
-data = np.loadtxt(results.file)
-print(data)
+# data = np.loadtxt(results.file)
+data = np.random.randint(10, size = 10)
 len = len(data)
 points = range(0, len)
+mode = []
+
 # Mean
 mean = np.mean(data)
 
 # Median
 if (len % 2 == 1):
-    median = sorted(data)[len / 2]
+    median = sorted(data)[len // 2]
 else:
     median = np.mean(sorted(data)[len // 2: len // 2 + 1])
 
@@ -29,11 +31,24 @@ for item in data:
     fq[item] += 1
 max_fq = np.max(list(fq.values()))
 i = 0
-while (fq[data[i]] != max_fq):
-    i += 1
-mode = data[i]
+for i in range(0, len):
+    if (fq[data[i]] == max_fq):
+        mode.extend([data[i]])
+mode = np.unique(mode)
 
+# Range
+range = np.max(data) - np.min(data)
+iqr = None
+if len >= 4:
+    middle_half = sorted(data)[len // 4: 3 * (len // 4)]
+    iqr = np.max(middle_half) - np.min(middle_half)
 fig = plt.figure()
+
+# Var
+var = np.var(data) # TODO Self implement
+
+# Standard deviation
+stddev = np.sqrt(var)
 
 # Subplot 1 - Scatter Plot
 subplot1 = fig.add_subplot(211)
@@ -58,7 +73,7 @@ subplot2.plot(fq.keys(), fq.values(), 'bo')
 sorted_fq = sorted(fq.items(), key=operator.itemgetter(0))
 subplot2.plot([elem[0] for elem in sorted_fq], [elem[1] for elem in sorted_fq], 'b-')
 
-subplot2.plot(mode, fq[mode], 'ro')
+subplot2.plot(mode, [fq[elem] for elem in mode], 'ro')
 mean_fq = np.mean(list(fq.values()))
 subplot2.axhline(y = mean_fq, color='r', linestyle='--')
 
@@ -67,9 +82,15 @@ plt.ylabel('Frequency')
 plt.title('Distribution Plot')
 subplot2.legend(['Frequencies', 'Frequency line', 'Mode', 'Mean frequency'])
 
-print('Mean: ', mean)
-print('Median: ', median)
-print('Mode: ', mode)
+print('Mean:\t\t\t', mean)
+print('Median:\t\t\t', median)
+print('Mode:\t\t\t', mode)
+print('Linear Regression:\t', fit[0], '* x +', fit[1])
+print('Correlation:\t\t', fit[0])
+print('Range:\t\t\t', range)
+print('Interquartile range:\t', iqr)
+print('Variance:\t\t', var)
+print('Standard deviation:\t', stddev)
 
 fig.tight_layout()
 plt.show()
